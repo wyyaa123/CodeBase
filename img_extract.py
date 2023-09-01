8246dfefb821f273a44b7cd878353451a9eaea93
wyyaa123 <863114354@qq.com>
2023-09-01 15:51:28 +0800

    '2023年9月1日'

--- img_extract.py
+++ img_extract.py
@@ -14,18 +14,20 @@ import rospy
 import cv2 as cv
 import numpy as np
 import tqdm
+import time
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 from cv_bridge import CvBridgeError
 
-path='./near/image/' #存放图片的位置
+path='./test/' #存放图片的位置
 class ImageCreator():
    def __init__(self):
-       self.bridge = CvBridge()
+        self.bridge = CvBridge()
     #    self.cnt = 8455
-       with rosbag.Bag('near.bag', 'r') as bag:   #要读取的bag文件；
-           for topic, msg, t in tqdm.tqdm(bag.read_messages()):
-                if topic == "rbg图topic":  #图像的topic；
+        with rosbag.Bag('near.bag', 'r') as bag:   #要读取的bag文件；
+            bar = tqdm.tqdm(bag.read_messages(), total=bag.get_message_count(), desc="ok!")
+            for topic, msg, t in bar:
+                if topic == "/iris_0/realsense/depth_camera/color/image_raw":  #rbg图像的topic；
                     try:
                         cv_image = self.bridge.imgmsg_to_cv2(msg,"bgr8")
                     except CvBridgeError as e:
@@ -39,10 +41,9 @@ class ImageCreator():
                     #     image_green = cv_image[:, :, 1]
                     #    image_red = cv_image[:, :, 2]
                     #    cv_image = np.dstack((image_blue, image_red, image_green)) #bgr->gbr
-                    flag = cv.imwrite(path + image_name, cv_image)  #保存；
-
-                    if flag: print('OK!')
-                    else: print('fail')
+                    
+                    if cv.imwrite(path + image_name, cv_image): bar.set_description_str("ok")  #保存；: print('OK!')
+                    else: bar.set_description_str("failed!")
                 if topic == "深度图topic" :
                     try:
                         depth_image = self.bridge.imgmsg_to_cv2(msg, "passthrough")
@@ -54,8 +55,8 @@ class ImageCreator():
                     name = timestr + ".npy" #图像命名：时间戳.
                     # name = "{0}".format(self.cnt) + ".npy"
                     np.save(path + name, depth_image)
-                   
-                self.cnt += 1
+
+                time.sleep(1)
                 
 
 if __name__ == '__main__':
