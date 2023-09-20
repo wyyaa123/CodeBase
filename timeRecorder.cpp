65847844405b08872d3a14bb1c13c52b3e13dc8c
wyyaa123 <863114354@qq.com>
2023-09-20 19:34:31 +0800

    '2023年9月20日'

---
+++ timeRecorder.cpp
@@ -0,0 +1,25 @@
+#include <chrono>
+#include <cstdio>
+#include <unistd.h> // Linux
+
+class TimeRecorder {
+public:
+    TimeRecorder() { start = std::chrono::high_resolution_clock::now(); }
+    double get_time() { return std::chrono::duration<double>(std::chrono::high_resolution_clock::now() - start).count(); }
+    void reset() {start = std::chrono::high_resolution_clock::now();}
+private:
+    std::chrono::_V2::system_clock::time_point start;
+};
+
+
+int main(int argc, char** argv) {
+
+    static TimeRecorder timer = TimeRecorder();
+    for (int i = 0; i != 10; ++i) {
+        printf("经过了 %.6f 秒\n", timer.get_time());
+        sleep(1);
+    }
+
+    return 0;
+}
+
